71a22e4e9f287668031edeeef4ba15b193233e77
Erick Simões <erick.simoes@live.com>
2021-09-05 00:12:49 -0300

    Create blink effect to TMP alpha text

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
@@ -8,13 +9,30 @@ public class GameController : MonoBehaviour {
     RectTransform circle;
     RectTransform canvas;
 
+    [SerializeField]
+    TextMeshProUGUI ClickHere;
+
     void Awake() {
         canvas = GetComponent<RectTransform>();
+    }
+
+    void Start() {
+        StartCoroutine(BlinkText());
     }
 
     public void OnClick() {
         float x = (canvas.rect.width / 2) - circle.rect.width;
         float y = (canvas.rect.height / 2) - circle.rect.height;
         circle.localPosition = new Vector3(Random.Range(-x, x), Random.Range(-y, y));
+    }
+
+    IEnumerator BlinkText() {
+        Color color = Color.white;
+        while(true) {
+            color.a = .2f + Mathf.PingPong(Time.time, .8f);
+            ClickHere.color = color;
+            yield return new WaitForSeconds(0.05f);
+        }
+        
     }
 }
